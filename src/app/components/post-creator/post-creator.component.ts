b7c6f018e150a74629c919a3d47d08595bc8d3f5
Duy Anh <dobadov3@dobadov3-MacBookPro.local>
2022-06-10 01:30:26 +0700

    fix bug

--- src/app/components/post-creator/post-creator.component.ts
+++ src/app/components/post-creator/post-creator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { PostCreatorDialogComponent } from '../dialogs/post-creator-dialog/post-creator-dialog.component';
 
@@ -8,9 +9,15 @@ import { PostCreatorDialogComponent } from '../dialogs/post-creator-dialog/post-
   styleUrls: ['./post-creator.component.scss'],
 })
 export class PostCreatorComponent implements OnInit {
+  formGroup: FormGroup;
+
   constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      content: new FormControl("", Validators.required)
+    })
+  }
 
   openDialog(): void {
     this.dialog.open(PostCreatorDialogComponent, {
